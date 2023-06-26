dd12abcb3ac37b7b8994676a10a3b4c2d2a9851a
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-06-26 20:56:33 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Ibrahim</h1>
+      </div>
+      <ul className="flex">
+        {links.map((link) => (
+          <li
+            key={link.id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link.link}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
